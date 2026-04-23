8529b3d4472aff6be92e0fe54d2e1b369b493ca5
AJ <juniormichieletto@gmail.com>
2026-04-23 22:39:13 +0100

    fix: use Java Toolchain for consistent JVM 21 targeting

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,9 +15,15 @@ subprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
     apply(plugin = "maven-publish")
 
+    configure<JavaPluginExtension> {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(21))
+        }
+    }
+
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         compilerOptions {
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
             freeCompilerArgs.add("-Xjsr305=strict")
         }
     }
