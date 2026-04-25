233a637132023d5113730b7bcbd55a68a63a8c3a
AJ <juniormichieletto@gmail.com>
2026-04-25 22:53:24 +0100

    Add publish CI and config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,13 @@
 plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.serialization) apply false
+    id("signing")
+    id("maven-publish")
 }
 
 allprojects {
-    group = "io.khrona"
-    version = "0.1.0-SNAPSHOT"
+    group = "io.github.juniormichieletto"
+    version = "0.1.0"
 
     repositories {
         mavenCentral()
@@ -15,11 +17,14 @@ allprojects {
 subprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
     apply(plugin = "maven-publish")
+    apply(plugin = "signing")
 
     configure<JavaPluginExtension> {
         toolchain {
             languageVersion.set(JavaLanguageVersion.of(21))
         }
+        withSourcesJar()
+        withJavadocJar()
     }
 
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
@@ -37,7 +42,45 @@ subprojects {
         publications {
             create<MavenPublication>("mavenJava") {
                 from(components["java"])
+                pom {
+                    name.set("Khrona")
+                    description.set("Coroutine-native job scheduling for Kotlin and Ktor")
+                    url.set("https://github.com/juniormichieletto/khrona")
+                    licenses {
+                        license {
+                            name.set("The Apache License, Version 2.0")
+                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("juniormichieletto")
+                            name.set("AJ")
+                            email.set("juniormichieletto@gmail.com")
+                        }
+                    }
+                    scm {
+                        connection.set("scm:git:git://github.com/juniormichieletto/khrona.git")
+                        developerConnection.set("scm:git:ssh://github.com:juniormichieletto/khrona.git")
+                        url.set("https://github.com/juniormichieletto/khrona")
+                    }
+                }
             }
+        }
+    }
+
+    configure<SigningExtension> {
+        // Use providers to safely read environment variables
+        val signingKeyBase64 = providers.environmentVariable("GCP_PRIVATE_KEY").getOrNull()
+        val signingPassword = providers.environmentVariable("GPG_PASSPHRASE").getOrNull()
+
+        if (!signingKeyBase64.isNullOrBlank()) {
+            val signingKey = String(java.util.Base64.getDecoder().decode(signingKeyBase64.trim()))
+            useInMemoryPgpKeys(signingKey, signingPassword)
+            sign(extensions.getByType<PublishingExtension>().publications["mavenJava"])
+        } else {
+            // Print a loud warning so you know immediately if the key wasn't picked up
+            logger.warn("⚠️ GPG_PRIVATE_KEY is missing! Artifacts will NOT be signed and Sonatype will reject them.")
         }
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,17 @@
+plugins {
+    id("com.gradleup.nmcp.settings") version "1.4.4"
+}
+
 rootProject.name = "khrona"
 
 include("khrona-core")
 include("khrona-ktor")
 include("khrona-store-memory")
 include("khrona-store-jdbc")
+
+nmcpSettings {
+    centralPortal {
+        username.set(providers.environmentVariable("OSSRH_USERNAME").getOrNull())
+        password.set(providers.environmentVariable("OSSRH_PASSWORD").getOrNull())
+    }
+}
